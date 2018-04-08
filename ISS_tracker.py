2acce29a245ba90895298d4d1be8a0a69e5212f2
ccggeo <georgeconnorc@gmail.com>
2018-04-08 16:04:51 +0100

    read geocoder status to output

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -7,44 +7,47 @@ import matplotlib.pyplot as plt
 
 def getTrackingdata():
 
-	response = urllib2.urlopen('http://api.open-notify.org/iss-now.json').read()
-	json_obj = str(response)
+    response = urllib2.urlopen('http://api.open-notify.org/iss-now.json').read()
+    json_obj = str(response)
 
-	data = json.loads(json_obj)
+    data = json.loads(json_obj)
 
-	time = data['timestamp']
-	timestamp = str(time)
+    time = data['timestamp']
+    timestamp = str(time)
 
-	position = data['iss_position']
-	longitude = position['longitude']
-	latitude = position['latitude']
-	try:
-		g= geocoder.google([latitude, longitude], method='reverse')
-		mapData = (g.raw)
-		country=(mapData['country'])
-		county = g.county
-	except:
-		country = ''
-		county=''
-	return longitude, latitude, timestamp, country, county
+    position = data['iss_position']
+    longitude = position['longitude']
+    latitude = position['latitude']
+
+    g= geocoder.google([latitude, longitude], method='reverse')
+    print(g.status)
+    try:
+        mapData = (g.raw)
+        country_=(mapData['country'])
+        country=str([country_['long_name']])
+        county = g.county
+    except:
+        country = ''
+        county=''
+    return longitude, latitude, timestamp, country, county
 
 
 geolocationData = getTrackingdata()
 def drawMap(longitude, latitude):
-	ax = plt.axes(projection=ccrs.Robinson())
-	plt.title('ISS Global Positioning %s' % geolocationData[2])
-	img=plt.imread('bm.png')
-	ax.imshow(img,origin='upper', transform=ccrs.PlateCarree())
-	#ax.gridlines()
+    ax = plt.axes(projection=ccrs.Robinson())
+    plt.title('ISS Global Positioning %s' % geolocationData[2])
+    img=plt.imread('bm.png')
+    ax.imshow(img,origin='upper', transform=ccrs.PlateCarree())
+    #ax.gridlines()
 
-	plt.plot(longitude, latitude, 'ro'
-		  ,transform=ccrs.PlateCarree(),
-		  )
+    plt.plot(longitude, latitude, 'ro'
+             ,transform=ccrs.PlateCarree(),
+             )
 
-	plt.text(longitude +3,  latitude -6, geolocationData[3] ,
-		  horizontalalignment='left', color='red',
-		  transform=ccrs.Geodetic())
+    plt.text(longitude +3,  latitude -6, geolocationData[3] ,
+             horizontalalignment='left', color='red',
+             transform=ccrs.Geodetic())
 
-	plt.show()
+    plt.show()
 
 drawMap(float(geolocationData[0]), float(geolocationData[1]))
