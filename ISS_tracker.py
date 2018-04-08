bb1527b39b7f26f55756e50edeb3f7ce78de1361
ccggeo <georgeconnorc@gmail.com>
2018-04-08 15:46:27 +0100

    Changed parsing of JSON in geocoder

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -3,50 +3,49 @@ import json
 import geocoder
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-
 #pip install geocoder
+
 def getTrackingdata():
 
-    response = urllib2.urlopen('http://api.open-notify.org/iss-now.json').read()
-    json_obj = str(response)
+	response = urllib2.urlopen('http://api.open-notify.org/iss-now.json').read()
+	json_obj = str(response)
 
-    data = json.loads(json_obj)
+	data = json.loads(json_obj)
 
-    time = data['timestamp']
-    timestamp = str(time)
+	time = data['timestamp']
+	timestamp = str(time)
 
-    position = data['iss_position']
+	position = data['iss_position']
+	longitude = position['longitude']
+	latitude = position['latitude']
+	try:
+		g= geocoder.google([latitude, longitude], method='reverse')
+		mapData = (g.raw)
+		country=(mapData['country'])
+		county = g.county
+		print(county)
+	except:
+		country = ''
+		county=''
+	return longitude, latitude, timestamp, country, county
 
-    longitude = position['longitude']
-    latitude = position['latitude']
-    #time related to position?
 
-    g = geocoder.google([latitude, longitude], method='reverse')
-    city = str(g.city)
-
-    if city == 'None':
-        print('Over ocean. No data')
-    else:
-        print('ISS is over ' + g.state_long + ', ' + g.country_long + ' ' + 'and the nearest city is ' + g.city)
-    return longitude, latitude, timestamp
-
-geolocation_data = getTrackingdata()
-
+geolocationData = getTrackingdata()
 def drawMap(longitude, latitude):
 	ax = plt.axes(projection=ccrs.Robinson())
-	plt.title('ISS Position %s' % geolocation_data[2])
+	plt.title('ISS Global Positioning %s' % geolocationData[2])
 	img=plt.imread('bm.png')
 	ax.imshow(img,origin='upper', transform=ccrs.PlateCarree())
 	#ax.gridlines()
 
 	plt.plot(longitude, latitude, 'ro'
-			,transform=ccrs.PlateCarree(),
-			)
+		  ,transform=ccrs.PlateCarree(),
+		  )
 
-	#plt.text(longitude +3,  latitude -12, 'iss position',
-		#	horizontalalignment='left',
-	#		transform=ccrs.Geodetic())
+	plt.text(longitude +3,  latitude -6, geolocationData[3] ,
+		  horizontalalignment='left', color='red',
+		  transform=ccrs.Geodetic())
 
 	plt.show()
 
-drawMap(float(geolocation_data[0]), float(geolocation_data[1]))
+drawMap(float(geolocationData[0]), float(geolocationData[1]))
